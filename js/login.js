5765d500be7a55fe9c83398a0a0068ac669a65a3
Cescastro <cesar.casrojas@gmail.com>
2017-03-12 20:01:16 -0500

    Init Struct

---
+++ js/login.js
@@ -0,0 +1,35 @@
+
+var btnLogin = document.getElementById('btnLogin');
+
+btnLogin.addEventListener("click",logginSubmit);
+
+function logginSubmit(e) {
+
+  event.preventDefault(e);
+  var userIn = document.getElementById('userIn').value;
+  var passIn = document.getElementById('passIn').value;
+  var isRoot = validarLoggin(userIn,passIn);
+  if (isRoot) {
+    console.log("Is Root User");
+  }else {
+    console.log("Is Not Root User");
+  }
+}
+
+function validarLoggin(userIn,passIn) {
+  if ((userIn == "yerman") && ( passIn == "yerman")) {
+    alert("WELLCOME YERMAN");
+    console.log(passIn,userIn)
+    return true;
+
+  }else if((userIn == "" && passIn == "")){
+    alert("WRITE SOMETHING");
+    console.log(passIn,userIn)
+    return false;
+
+    }else {
+      alert("U ARE'NT YERMAN");
+      console.log(passIn,userIn)
+      return false;
+  }
+}
