39fe450d9ae9871fb26e6cee047feda1fc63a772
Milton Castañeda <milton1792@gmail.com>
2017-03-31 10:56:27 -0500

    Menu y boton de menu para header, estructura inicial con manejo de gestos tactiles mediante hammer.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,50 @@
+/*
+  Controlar los gestos tactiles del menu,
+  mediante la libreria hammer.js 
+*/
 
-$(document).ready(function(){
-  console.log("JQUERY");
-});
+const menu = document.getElementById('dashboard-menu')
+const logoIconClose = document.getElementById('menu-button-icon-img-close')  
+const logoIconOpen = document.getElementById('menu-button-icon-img-open')
+
+const menuInit = () => {
+
+  let hammerMenu = new Hammer(document,{})
+  
+  hammerMenu.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
+  hammerMenu.on('swipe',menuAction)
+
+  let hammerMenuIconClose = new Hammer(logoIconClose,{})
+  hammerMenuIconClose.on('tap',closedMenu)
+
+  let hammerMenuIconOpen = new Hammer(logoIconOpen,{})
+  hammerMenuIconOpen.on('tap',openMenu)
+}
+
+const menuAction = (e) => {
+
+  if( e.deltaX < -20 ) closedMenu()
+  if( e.deltaX > 20 ) openMenu()
+
+}
+
+const closedMenu = () => {
+
+  menu.style.transition = '1s'
+  menu.style.left = '-150%'
+  
+}
+
+const openMenu = () => {
+
+  menu.style.transition = '1s'
+  menu.style.left = '0'
+
+}
+
+menuInit()
+
+
+
+
+
